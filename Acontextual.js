171a55ff9ffb674f9e7d2e46a2aa1153cb2a272f
Josue <victorjosue1260@gmail.com>
2017-09-07 16:11:33 -0600

    archivos agregados del analisis contextual

---
+++ Acontextual.js
@@ -0,0 +1,24 @@
+var antlr4 = require('antlr4/index');
+var CParser = require('generated/CParser');
+var CScanner = require('generated/CScanner');
+var Acontextual = require('JavaScripts/VisitorAContextual');
+
+
+
+function analisisSintactico() {
+
+    editor = $('textarea').data('ace-editor');
+
+    var textArea = document.getElementById('consola');
+    textArea.Value = "";
+    var input = editor.getValue();//document.getElementById('contenidoarchivo').Value;
+    var chars = new antlr4.InputStream(input);
+    var lexer = new CScanner.CScanner(chars);
+    var tokens = new antlr4.CommonTokenStream(lexer);
+    var parser = new CParser.CParser(tokens);
+    parser.buildParseTrees = true;
+    var tree = parser.program();
+
+
+
+}
